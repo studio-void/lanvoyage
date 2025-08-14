5fe08cf58b343414306b378bd1e7b749f8097749
Cyan <hoony6134@gmail.com>
2025-08-14 19:23:31 +0900

    chore: boilerplate

---
+++ lanvoyage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lanvoyage
+//
+//  Created by 임정훈 on 8/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lanvoyage/lanvoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  lanvoyageApp.swift
+//  lanvoyage
+//
+//  Created by 임정훈 on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct lanvoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
