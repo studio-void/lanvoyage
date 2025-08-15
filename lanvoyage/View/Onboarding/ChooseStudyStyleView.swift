4b82b1138aa5e5dd8c2fbf38a23de8b477eb34d1
Cyan <hoony6134@gmail.com>
2025-08-15 14:01:58 +0900

    fix: navigation on onboarding step 1,2

--- lanvoyage/View/Onboarding/ChooseStudyStyleView.swift
+++ lanvoyage/View/Onboarding/ChooseStudyStyleView.swift
@@ -13,19 +13,25 @@ struct ChooseStudyStyleView: View {
     @State private var selectedStyles: Set<String> = []
     @State var targetPeriod: TargetPeriod?
     @State private var selectedPeriod: Set<String> = []
-    
+    @Environment(\.presentationMode) var presentationMode
+
     private let columns: [GridItem] = [
         GridItem(.flexible(), spacing: 16),
-        GridItem(.flexible(), spacing: 16)
+        GridItem(.flexible(), spacing: 16),
     ]
-    
+
     var body: some View {
-        VStack{
-            HStack{
-                VoidStepsView(currentStep: 2, totalSteps: 2, tintColor: Color.violet500, disabledOpacity: 0.3)
+        VStack {
+            HStack {
+                VoidStepsView(
+                    currentStep: 2,
+                    totalSteps: 2,
+                    tintColor: Color.violet500,
+                    disabledOpacity: 0.3
+                )
                 Spacer()
             }
-            .padding(.bottom,8)
+            .padding(.bottom, 8)
             HStack {
                 Text("어떤 학습 스타일이 편하신가요?")
                     .font(.title)
@@ -33,11 +39,11 @@ struct ChooseStudyStyleView: View {
                 Spacer()
             }
             .padding(.bottom, 2)
-            
+
             HStack {
                 Text("중복 선택이 가능해요.")
                 Spacer()
-                
+
                 Text("2/2")
                     .foregroundStyle(Color.gray500)
             }
@@ -45,16 +51,20 @@ struct ChooseStudyStyleView: View {
             .padding(.bottom)
             LazyVGrid(columns: columns, alignment: .center, spacing: 12) {
                 ForEach(StudyStyle.allCases, id: \.rawValue) { item in
-                    SelectionChipView(label: item.rawValue, isSelected: Binding<Bool>(
-                        get: { selectedStyles.contains(item.rawValue) },
-                        set: { isSelected in
-                            if isSelected {
-                                selectedStyles.insert(item.rawValue)
-                            } else {
-                                selectedStyles.remove(item.rawValue)
+                    SelectionChipView(
+                        label: item.rawValue,
+                        isSelected: Binding<Bool>(
+                            get: { selectedStyles.contains(item.rawValue) },
+                            set: { isSelected in
+                                if isSelected {
+                                    selectedStyles.insert(item.rawValue)
+                                } else {
+                                    selectedStyles.remove(item.rawValue)
+                                }
                             }
-                        }
-                    ), tintColor: Color.violet500)
+                        ),
+                        tintColor: Color.violet500
+                    )
                     .padding(.horizontal, 0.8)
                 }
                 .onChange(of: selectedStyles) {
@@ -62,30 +72,34 @@ struct ChooseStudyStyleView: View {
                 }
             }
             .padding(.bottom)
-            HStack{
+            HStack {
                 Text("목표 기간")
                     .font(.title2)
                     .fontWeight(.bold)
                 Spacer()
             }
-            .padding(.bottom,8)
+            .padding(.bottom, 8)
             ForEach(TargetPeriod.allCases, id: \.rawValue) { item in
-                HStack{
-                    CheckCircleSelectionView(label: item.rawValue, isSelected: Binding<Bool>(
-                        get: { selectedPeriod.contains(item.rawValue) },
-                        set: { isSelected in
-                            if isSelected {
-                                selectedPeriod.insert(item.rawValue)
-                            } else {
-                                selectedPeriod.remove(item.rawValue)
+                HStack {
+                    CheckCircleSelectionView(
+                        label: item.rawValue,
+                        isSelected: Binding<Bool>(
+                            get: { selectedPeriod.contains(item.rawValue) },
+                            set: { isSelected in
+                                if isSelected {
+                                    selectedPeriod.insert(item.rawValue)
+                                } else {
+                                    selectedPeriod.remove(item.rawValue)
+                                }
                             }
-                        }
-                    ), tintColor: Color.violet500)
+                        ),
+                        tintColor: Color.violet500
+                    )
                     .scaleEffect(1.2, anchor: .leading)
                     .padding(.horizontal, 0.8)
                     Spacer()
                 }
-                .padding(.bottom,2)
+                .padding(.bottom, 2)
             }
             .onChange(of: selectedStyles) {
                 print("selectedStyles: \(selectedStyles)")
@@ -93,21 +107,35 @@ struct ChooseStudyStyleView: View {
             Spacer()
             GeometryReader { proxy in
                 let spacing: CGFloat = 12
-                let w1 = (proxy.size.width - spacing) / 3          // "이전" 1/3
-                let w2 = (proxy.size.width - spacing) * 2 / 3      // "다음" 2/3
+                let w1 = (proxy.size.width - spacing) / 3  // "이전" 1/3
+                let w2 = (proxy.size.width - spacing) * 2 / 3  // "다음" 2/3
 
                 HStack(spacing: spacing) {
-                    CustomButtonView(title: "이전", kind: .outline) {}
-                        .frame(width: w1)
-                        .padding(.leading, 0.2)
-                    NavigationLink(destination: ChooseStudyStyleView()){
-                        CustomButtonView(title: "다음", kind: (selectedStyles.isEmpty || selectedPeriod.count != 1) ? .disabled : .filled)
-                            .frame(width: w2)
-                            .padding(.trailing, 0.2)
-                    }.disabled(selectedStyles.isEmpty || selectedPeriod.count != 1)
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        CustomButtonView(title: "이전", kind: .outline) {}
+                            .frame(width: w1)
+                            .padding(.leading, 0.2)
+                    }
+                    NavigationLink(
+                        destination: ChooseStudyStyleView()
+                            .navigationBarBackButtonHidden(true)
+                    ) {
+                        CustomButtonView(
+                            title: "다음",
+                            kind: (selectedStyles.isEmpty
+                                || selectedPeriod.count != 1)
+                                ? .disabled : .filled
+                        )
+                        .frame(width: w2)
+                        .padding(.trailing, 0.2)
+                    }.disabled(
+                        selectedStyles.isEmpty || selectedPeriod.count != 1
+                    )
                 }
             }
-            .frame(height:49)
+            .frame(height: 49)
         }
     }
 }
