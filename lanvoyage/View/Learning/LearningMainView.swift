34a266c954f4dcab2d21b071f9d31a0bddd77671
Cyan <hoony6134@gmail.com>
2025-08-15 22:19:48 +0900

    chore: file structure

---
+++ lanvoyage/View/Learning/LearningMainView.swift
@@ -0,0 +1,18 @@
+//
+//  LearningMainView.swift
+//  lanvoyage
+//
+//  Created by 임정훈 on 8/15/25.
+//
+
+import SwiftUI
+
+struct LearningMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LearningMainView()
+}
